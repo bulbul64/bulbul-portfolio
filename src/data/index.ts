97fb1fed9512f3835e07d4ad3b766258e8052fe8
bulbul64 <bbb46483@gmail.com>
2026-05-15 17:03:25 +0600

    fix: tools module export issue

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,5 @@ export * from './nav';
 export * from './projects';
 export * from './services';
 export * from './experience';
-export * from './tools';
 export * from './stats';
 export * from './testimonials';
